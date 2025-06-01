2ebe40736972fbc7ed17ed93605e12cdf533354a
sophieHerstein <65544749+sophieHerstein@users.noreply.github.com>
2025-06-01 23:52:20 +0200

    removed routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SceneComponent} from "./components/scene/scene.component";
 
@@ -11,8 +10,7 @@ import {SceneComponent} from "./components/scene/scene.component";
     SceneComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
